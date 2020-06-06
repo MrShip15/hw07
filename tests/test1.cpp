1375d2898e8ac28ccae1b4864b7cb905811e7662
MrShip15 <61249047+MrShip15@users.noreply.github.com>
2020-06-06 18:05:59 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -3,25 +3,25 @@
 // Тест на проверку правильности конструктора
 TEST(Account, Constructor)
 {
-Account a(2,300);
+Account a(5,400);
 
-EXPECT_EQ(a.id(),2);
-EXPECT_EQ(a.GetBalance(),300);
+EXPECT_EQ(a.id(),5);
+EXPECT_EQ(a.GetBalance(),400);
 }
 // Тест на проверку правильность изменения баланса
 TEST(Account, ChangeBalance)
 {
-  Account a(2,300);
+  Account a(5,400);
   a.Lock();
   a.ChangeBalance(100);
-  EXPECT_EQ(a.GetBalance(),400);
+  EXPECT_EQ(a.GetBalance(),500);
 }
 // Тест на проверку состояния аккаунта(открыт/закрыт)
 TEST(Account, Lock)
 {
-  Account a(2,300);
+  Account a(5,400);
   a.Lock();
   a.ChangeBalance(100);
   a.Unlock();
-  EXPECT_EQ(a.GetBalance(),400);
+  EXPECT_EQ(a.GetBalance(),500);
 }
