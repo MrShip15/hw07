bb3582032398be43e7097a0e98922a209a2044cb
MrShip15 <mr_ship2001@mail.ru>
2020-06-08 21:39:56 +0300

    hw07

---
+++ _builds/CMakeFiles/check.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ivan/MrShip15/workspace/projects/hw07/tests/test1.cpp" "/home/ivan/MrShip15/workspace/projects/hw07/_builds/CMakeFiles/check.dir/tests/test1.cpp.o"
+  "/home/ivan/MrShip15/workspace/projects/hw07/tests/test2.cpp" "/home/ivan/MrShip15/workspace/projects/hw07/_builds/CMakeFiles/check.dir/tests/test2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../banking"
+  "/home/ivan/.hunter/_Base/5659b15/9b2c9d4/8a1641b/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ivan/MrShip15/workspace/projects/hw07/_builds/CMakeFiles/account.dir/DependInfo.cmake"
+  "/home/ivan/MrShip15/workspace/projects/hw07/_builds/CMakeFiles/transaction.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ivan/MrShip15/workspace/projects/hw07
+# Build directory: /home/ivan/MrShip15/workspace/projects/hw07/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/ivan/MrShip15/workspace/projects/hw07/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/ivan/MrShip15/workspace/projects/hw07/CMakeLists.txt;34;add_test;/home/ivan/MrShip15/workspace/projects/hw07/CMakeLists.txt;0;")
